b246235ebf3e578a0dfc7c4129ab793bafb9ab49
elkart <elkin.arteaga@gmail.com>
2022-11-16 10:17:50 -0500

    COMMIT 3: Cambio BD

--- database.js
+++ database.js
@@ -3,7 +3,8 @@ const mongo = require("mongoose");
 //CONFIGURAR CONEXIÓN:
 (async () => {
     try {
-        const db = await mongo.connect("mongodb://localhost:27017/dulces-db");
+        //const db = await mongo.connect("mongodb://localhost:27017/dulces-db");
+        const db = await mongo.connect("mongodb+srv://elkart:3OpxYyJRXdhIZVEL@cluster0.rb8nnfe.mongodb.net/my_database?retryWrites=true&w=majority");
         //comprobar la conexión:
         console.log("Conexión establecida en: ", db.connection.name);
     } catch (error) {
